f64bd0dc960f6af52ba6f8d45dfd1903e31b0e30
Jonathan Bowman <git@bowmanjd.com>
2023-03-20 19:50:00 -0400

    feat: added special first line handling

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use anyhow::{Context, Result};
 use csv::{Reader, ReaderBuilder, StringRecord};
 use serde_json::Map;
 use std::fs::File;
-use std::io::{BufWriter, Seek, SeekFrom, Write};
+use std::io::{BufWriter, Write};
 use std::path::PathBuf;
 
 fn csv_reader(csvfile: &PathBuf) -> Result<Reader<File>> {
@@ -33,23 +33,47 @@ pub fn csv_to_json(csvfile: &PathBuf, jsonfile: &PathBuf) -> Result<()> {
     let outfile = File::create(jsonfile)?;
     let mut stream = BufWriter::new(outfile);
     let headers = rdr.headers()?.clone();
+    let mut first_line = true;
 
     write!(stream, "[\n")?;
 
     for result in rdr.records() {
+        if first_line == false {
+            write!(stream, ",\n")?;
+        } else {
+            first_line = false;
+        }
         let row = result?;
         let mut json_row = Map::new();
         let row_length: usize = row.len();
         for i in 0..row_length {
-            let column = headers.get(i).expect("JSON column");
-            let value = row.get(i).expect("JSON value");
+            let column = &headers[i];
+            let value = &row[i];
             json_row.insert(column.into(), value.into());
         }
         let json_text = serde_json::to_string(&json_row)?;
-        write!(stream, "{},\n", json_text)?;
+        write!(stream, "{}", json_text)?;
     }
-    stream.seek(SeekFrom::Current(-2))?;
     write!(stream, "\n]")?;
+    stream.flush()?;
+    Ok(())
+}
+
+pub fn csv_to_bcp(csvfile: &PathBuf, bcpfile: &PathBuf) -> Result<()> {
+    let mut rdr = csv_reader(csvfile)?;
+    let outfile = File::create(bcpfile)?;
+    let mut stream = BufWriter::new(outfile);
+    let mut first_line = true;
+    for result in rdr.records() {
+        if first_line == false {
+            write!(stream, "\x1E")?;
+        } else {
+            first_line = false;
+        }
+        let row = result?;
+        let line = row.iter().collect::<Vec<&str>>().join("\x1F");
+        write!(stream, "{}", line)?;
+    }
     stream.flush()?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,23 @@ struct Args {
 
     /// output JSON
     #[argh(option, short = 'j')]
-    jsonfile: PathBuf,
+    jsonfile: Option<PathBuf>,
+    ///
+    /// output BCP
+    #[argh(option, short = 'b')]
+    bcpfile: Option<PathBuf>,
 }
 
 fn run() -> Result<()> {
     let args: Args = argh::from_env();
     let headers = busser::read_csv_headers(&args.csvfile)?;
     println!("{}", headers.join(", "));
-    busser::csv_to_json(&args.csvfile, &args.jsonfile)?;
+    if let Some(jsonfile) = args.jsonfile {
+        busser::csv_to_json(&args.csvfile, &jsonfile)?;
+    }
+    if let Some(bcpfile) = args.bcpfile {
+        busser::csv_to_bcp(&args.csvfile, &bcpfile)?;
+    }
     Ok(())
 }
 
